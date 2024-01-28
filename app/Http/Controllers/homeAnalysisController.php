de95b5a91268f58cad77026a2ce67365169cdf65
mohamed zakarya <MOHAMED ZAKARYA>
2024-01-28 12:46:33 +0200

    all done

--- app/Http/Controllers/homeAnalysisController.php
+++ app/Http/Controllers/homeAnalysisController.php
@@ -12,6 +12,27 @@ class homeAnalysisController extends Controller
 
     public function HomeAnalysis() {
 
+
+        $total_25_taken = Apology::where('Status' , 0)->where('Amount' , 25)->count();
+        $total_25_avaliable = Apology::where('Status' , 1)->where('Amount' , 25)->count();
+        $total_50_taken =Apology::where('Status' , 0)->where('Amount' , 50)->count();
+        $total_50_avaliable =Apology::where('Status' , 1)->where('Amount' , 50)->count();
+        $total_100_taken = Apology::where('Status' , 0)->where('Amount' , 100)->count();
+        $total_100_avaliable = Apology::where('Status' , 1)->where('Amount' , 100)->count();
+        $total_150_taken = Apology::where('Status' , 0)->where('Amount' , 150)->count();
+        $total_150_avaliable = Apology::where('Status' , 1)->where('Amount' , 150)->count();
+        $total_200_taken = Apology::where('Status' , 0)->where('Amount' , 200)->count();
+        $total_200_avaliable = Apology::where('Status' , 1)->where('Amount' , 200)->count();
+        $total_300_taken = Apology::where('Status' , 0)->where('Amount' , 300)->count();
+        $total_300_avaliable =Apology::where('Status' , 1)->where('Amount' , 300)->count();
+        $total_400_taken = Apology::where('Status' , 0)->where('Amount' , 400)->count();
+        $total_400_avaliable =Apology::where('Status' , 1)->where('Amount' , 400)->count();
+        $total_500_taken = Apology::where('Status' , 0)->where('Amount' , 500)->count();
+        $total_500_avaliable = Apology::where('Status' , 1)->where('Amount' , 500)->count();
+       
+
+
+
         $chartjs = app()->chartjs
         ->name('pieChartTest')
         ->type('pie')
@@ -30,7 +51,7 @@ class homeAnalysisController extends Controller
         ->name('lineChartTest')
         ->type('line')
         ->size(['width' => 400, 'height' => 200])
-        ->labels(['January', 'February', 'March', 'April', 'May', 'June', 'July'])
+        ->labels(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
         ->datasets([
             [
                 "label" => "My First dataset",
@@ -51,11 +72,44 @@ class homeAnalysisController extends Controller
                 "pointHoverBackgroundColor" => "#fff",
                 "pointHoverBorderColor" => "rgba(220,220,220,1)",
                 'data' => [12, 33, 44, 44, 55, 23, 40],
+            ],
+            [
+                "label" => "My Second dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [13, 40, 30, 20, 60, 80, 10],
+            ],
+            [
+                "label" => "My Second dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [3, 3, 3, 2, 3, 3, 0],
+            ],
+            [
+                "label" => "My Second dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [2, 2, 2, 2, 2, 2, 2],
             ]
         ])
         ->options([]);
 
-        return view('dashboard.index' , compact('chartjs' , 'chartjs_2'));
+        return view('dashboard.index' , compact('chartjs' , 'chartjs_2' , 'total_25_taken' ,'total_25_avaliable', 'total_50_taken' 
+        ,'total_50_avaliable' , 'total_100_taken' ,'total_100_avaliable' , 'total_150_taken' ,'total_150_avaliable' 
+        , 'total_200_taken' ,'total_200_avaliable' , 'total_300_taken' ,'total_300_avaliable' , 'total_400_taken' 
+        ,'total_400_avaliable' , 'total_500_taken' ,'total_500_avaliable' ));
     }
 
 
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -40,12 +40,16 @@
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_25_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count taken</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -23.09%</span>
+											<span class="text-white op-7"> +
+												{{$total_25_avaliable}}
+											</span>
 										</span>
 									</div>
 								</div>
@@ -54,7 +58,7 @@
 						</div>
 					</div>
 					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-						<div class="card overflow-hidden sales-card bg-warning-gradient">
+						<div class="card overflow-hidden sales-card bg-danger-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
 									<h6 class="mb-3 tx-12 text-white">Apology 50</h6>
@@ -62,17 +66,21 @@
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_50_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -152.3</span>
+											<span class="text-white op-7"> +
+												{{$total_50_avaliable}}
+											</span>
 										</span>
 									</div>
 								</div>
 							</div>
-							<span class="pt-1 compositeline4">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+							<span  class="pt-1 compositeline">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
 						</div>
 					</div>
 					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
@@ -84,12 +92,42 @@
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_100_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -152.3</span>
+											<span class="text-white op-7"> +
+												{{$total_100_avaliable}}
+											</span>
+										</span>
+									</div>
+								</div>
+							</div>
+							<span class="pt-1 compositeline4">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+						<div class="card overflow-hidden sales-card bg-warning-gradient">
+							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
+								<div class="">
+									<h6 class="mb-3 tx-12 text-white">Apology 150</h6>
+								</div>
+								<div class="pb-0 mt-0">
+									<div class="d-flex">
+										<div class="">
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_150_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
+										</div>
+										<span class="float-right my-auto mr-auto">
+											<i class="fas fa-arrow-circle-down text-white"></i>
+											<span class="text-white op-7"> +
+												{{$total_150_avaliable}}
+											</span>
 										</span>
 									</div>
 								</div>
@@ -101,17 +139,21 @@
 						<div class="card overflow-hidden sales-card bg-primary-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">Apology 150</h6>
+									<h6 class="mb-3 tx-12 text-white">Apology 200</h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$5,74.12</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_200_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-up text-white"></i>
-											<span class="text-white op-7"> +427</span>
+											<span class="text-white op-7"> +
+												{{$total_200_avaliable}} 
+											</span>
 										</span>
 									</div>
 								</div>
@@ -120,29 +162,7 @@
 						</div>
 					</div>
 					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-						<div class="card overflow-hidden sales-card bg-danger-gradient">
-							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
-								<div class="">
-									<h6 class="mb-3 tx-12 text-white">Apology 200</h6>
-								</div>
-								<div class="pb-0 mt-0">
-									<div class="d-flex">
-										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
-										</div>
-										<span class="float-right my-auto mr-auto">
-											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -23.09%</span>
-										</span>
-									</div>
-								</div>
-							</div>
-							<span  class="pt-1 compositeline">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
-						</div>
-					</div>
-					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-						<div class="card overflow-hidden sales-card bg-success-gradient">
+						<div class="card overflow-hidden sales-card bg-primary-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
 									<h6 class="mb-3 tx-12 text-white">Apology 300</h6>
@@ -150,12 +170,42 @@
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$7,125.70</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_300_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-up text-white"></i>
-											<span class="text-white op-7"> 52.09%</span>
+											<span class="text-white op-7"> +
+												{{$total_300_avaliable}}
+											</span>
+										</span>
+									</div>
+								</div>
+							</div>
+							<span class="pt-1 compositeline">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+						<div class="card overflow-hidden sales-card bg-success-gradient">
+							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
+								<div class="">
+									<h6 class="mb-3 tx-12 text-white">Apology 400</h6>
+								</div>
+								<div class="pb-0 mt-0">
+									<div class="d-flex">
+										<div class="">
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_400_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
+										</div>
+										<span class="float-right my-auto mr-auto">
+											<i class="fas fa-arrow-circle-up text-white"></i>
+											<span class="text-white op-7"> +
+												{{$total_400_avaliable}}
+											</span>
 										</span>
 									</div>
 								</div>
@@ -164,29 +214,7 @@
 						</div>
 					</div>
 					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-						<div class="card overflow-hidden sales-card bg-warning-gradient">
-							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
-								<div class="">
-									<h6 class="mb-3 tx-12 text-white">Apology 400</h6>
-								</div>
-								<div class="pb-0 mt-0">
-									<div class="d-flex">
-										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
-										</div>
-										<span class="float-right my-auto mr-auto">
-											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -152.3</span>
-										</span>
-									</div>
-								</div>
-							</div>
-							<span class="pt-1 compositeline4">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
-						</div>
-					</div>
-					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-						<div class="card overflow-hidden sales-card bg-warning-gradient">
+						<div class="card overflow-hidden sales-card bg-success-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
 									<h6 class="mb-3 tx-12 text-white">Apology 500</h6>
@@ -194,17 +222,21 @@
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{$total_500_taken}}
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">Count Taken</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
-											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -152.3</span>
+											<i class="fas fa-arrow-circle-up text-white"></i>
+											<span class="text-white op-7"> +
+												{{$total_500_avaliable}}
+											</span>
 										</span>
 									</div>
 								</div>
 							</div>
-							<span class="pt-1 compositeline4">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+							<span class="pt-1 compositeline">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span>
 						</div>
 					</div>
 				</div>
